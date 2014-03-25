83352107eaaba03c8827e29f1d0e50c12371310d
George Czabania <george@czabania.com>
2014-03-25 17:32:18 +1300

    Get gulp to ignore errors

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,36 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
+var connect = require('gulp-connect');
 
 gulp.task('default', ['sass', 'scripts']);
 
 gulp.task('watch', function () {
-  gulp.watch('stylesheets/*/*.scss', ['sass']);
-  gulp.watch('scripts/client/*/*js', ['scripts']);
+  gulp.watch('stylesheets/**/*.scss', ['sass']);
+  gulp.watch('scripts/client/**/*.js', ['scripts']);
 });
 
+gulp.task('connect', ['watch'], connect.server({
+  root: ['dist'],
+  port: 8080,
+  livereload: true
+}));
+
 gulp.task('sass', function () {
+
+  var _sass = sass();
+  _sass.on('error', console.log.bind(console));
+
   gulp.src('stylesheets/main.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(_sass)
+    .pipe(gulp.dest('dist/css'))
+    .pipe(connect.reload());
 });
 
 gulp.task('scripts', function () {
   gulp.src('scripts/client/index.js')
     .pipe(browserify())
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('dist/js'))
+    .pipe(connect.reload());
 });
