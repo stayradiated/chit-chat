4d86af4a8285cdb70f1a09f1cd3278a2b18393de
George Czabania <george@czabania.com>
2014-03-27 23:18:47 +1300

    Start developing server

---
+++ scripts/server/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var Jandal = require('jandal');
+var sockjs = require('sockjs');
+var express = require('express');
+var _ = require('underscore');
+
+var app = express();
+
+app.configure(function () {
+  app.use(express.static(__dirname + '/../../dist'));
+});
+
+var server = app.listen(8080);
+
+var connection = sockjs.createServer();
+connection.installHandlers(server, { prefix: '/socket' });
+
+connection.on('connection', function (socket) {
+  var jandal = new Jandal(socket, 'stream');
+  new Socket(jandal);
+});
+
+var Socket = function (socket) {
+  this.socket = socket;
+};
+
+_.extend(Socket.prototype, {
+
+
+
+});
+
+var rooms = [];
